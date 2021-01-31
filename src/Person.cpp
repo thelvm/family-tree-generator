0fd181e0138602c6a3093baf2a6f37f3e8ac9e12
thelvm <l.viegasmilani@gmail.com>
2021-01-31 12:07:08 +0100

    Added weak reference to direct ancestors and decendents

--- src/Person.cpp
+++ src/Person.cpp
@@ -4,3 +4,13 @@ Person::Person()
 {
     id = ++globalCount;
 }
+
+std::map<int, std::weak_ptr<Person>> Person::getDirectDecedents()
+{
+    return directDecedents;
+}
+
+std::map<int, std::weak_ptr<Person>> Person::getDirectAncestors()
+{
+    return directAncestors;
+}
--- src/include/Person.hpp
+++ src/include/Person.hpp
@@ -9,9 +9,14 @@ class Person
 private:
     static int globalCount;
     int id;
+
     std::map<int, std::weak_ptr<Person>> directDecedents;
+    std::map<int, std::weak_ptr<Person>> directAncestors;
+
 public:
     Person();
+    std::map<int, std::weak_ptr<Person>> getDirectDecedents();
+    std::map<int, std::weak_ptr<Person>> getDirectAncestors();
 };
 
 
