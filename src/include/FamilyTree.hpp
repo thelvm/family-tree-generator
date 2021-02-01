75a8f7d56bd8eb531a22eaef0b71df9aa26a1e7c
Lucas Viegas Milani <l.viegasmilani@gmail.com>
2021-02-01 11:42:55 +0100

    Scaffolded part of FamilyTree

--- src/include/FamilyTree.hpp
+++ src/include/FamilyTree.hpp
@@ -6,10 +6,25 @@
 #include <set>
 #include <memory>
 
+typedef struct familyTreeGenerationParameters
+{
+    int generationsBefore;
+    int generationsAfter;
+    // Fertility rate?
+} FamilyTreeGenerationParameters;
+
 class FamilyTree
 {
 private:
     std::set<std::shared_ptr<Person>> members;
+
+public:
+    /// Sets the generation parameters for the family tree. To be used before generate()
+    void setGenerationParameters(const FamilyTreeGenerationParameters &generationParameters);
+    /// Generates the family tree using the previously set generation parameters;
+    void generate();
+    /// Generates the family tree using the given parameters, ignoring any previously set parameters
+    void generate(const FamilyTreeGenerationParameters &generationParameters);
 };
 
 #endif //FAMILYTREE_HPP
